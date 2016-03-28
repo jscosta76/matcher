bd14556c12b44f4ef9821aaed1dd68eeb1053fc3
Julio Costa <julio.costa@webdetails.pt>
2016-03-28 22:05:37 +0100

    Update User Model
    - Include some route validation for saving user

--- server.js
+++ server.js
@@ -23,6 +23,47 @@ router.get('/', function(req, res){
 	res.json({message:'API is up and running'});
 });
 
+router.route('/user')
+	.post(function(req, res){
+		var user = new User();
+		
+		user.firstName = req.body.firstName;
+		user.lastName = req.body.lastName;
+		user.displayName = req.body.displayName;
+		user.password = req.body.password;	
+		user.email = req.body.email;
+	
+		user.save(function(err){
+			if(err){
+				if(err.code===11000){
+					//res.redirect('/user/new?exists=true');
+					console.log('email already exists');
+				} else {
+					//res.redirect('?error=true');
+					console.log('some other error occured');
+				}
+			} else {
+				console.log('User successfully saved: ' + user);
+				/*req.session.user = {
+					"name": user.firstName+' '+user.lastName,
+					"email":user.email,
+					"_id":user._id
+				};
+				req.session.loggedIn = true;
+				res.redirect('/user');*/
+			}
+		});
+	})
+	.get(function(req, res){
+		User.find(function(err, users){
+			if(err){
+				res.send(err);
+			}
+			
+			res.json(users);
+		});
+	});
+
 app.use('/api', router);
 
 app.listen(port);
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,52 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var UserSchema = new Schema({});
+var UserSchema = new Schema({
+	firstName:{
+		type:String,
+		required:true,
+		trim:true
+	},
+	lastName:{
+		type:String,
+		required:true,
+		trim:true
+	},
+	displayName:{
+		type:String,
+		required:true,
+		trim:true
+	},
+	password:String,
+	email:{
+		type:String,
+		required:true,
+		unique:true,
+		trim:true
+	},
+	/*
+	approved:{
+		type:Boolean,
+		default:false
+	},
+	banned:{
+		type:Boolean,
+		default:false
+	},
+	admin:{
+		type:Boolean,
+		default:false
+	},*/
+	headline:String,
+	photoUrl:String,
+	created:{
+		type:Date,
+		default:Date.now
+	},
+	updated:{
+		type:Date,
+		default:Date.now
+	}
+});
 
 module.exports = mongoose.model('User', UserSchema);
